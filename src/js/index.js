61e4d106e1f8930a75493845e42d65be2ea6bbe4
felipeog <felipeog476@gmail.com>
2020-11-05 14:12:13 -0300

    Set map view and marker

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,27 @@
 // console.log('{{IPIFY_TOKEN}}')
 // console.log('{{MAPBOX_TOKEN}}')
 
-const map = L.map('mapid').setView([51.505, -0.09], 13)
+const map = L.map('mapid')
+const marker = L.marker([0, 0]).addTo(map)
+const layer = L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: '{{MAPBOX_TOKEN}}',
+  },
+).addTo(map)
+
+window.addEventListener('load', () => {
+  fetch('https://geo.ipify.org/api/v1?apiKey={{IPIFY_TOKEN}}')
+    .then(res => res.json())
+    .then(({ location: { lat, lng } }) => {
+      map.setView([lat, lng], 13)
+      marker.setLatLng(new L.LatLng(lat, lng))
+    })
+    .catch(err => console.error(err))
+})
